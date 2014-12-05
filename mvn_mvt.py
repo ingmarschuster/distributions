a7af4cc9ee9cdad91ab66bc91272bf17908a8982
ingmarschuster <ingmar.schuster.linguistics@gmail.com>
2014-12-05 10:14:56 +0100

    MVN and MVT: collapsed calculation of likelihood and gradient for efficiency gain

--- mvn_mvt.py
+++ mvn_mvt.py
@@ -11,13 +11,18 @@ from linalg import pdinv
 
 class mvnorm(object):
     def __init__(self, mu, K): 
-        mu = np.atleast_1d(mu)
-        K = np.atleast_2d(K)       
+        mu = np.atleast_1d(mu).flat[:]
+        K = np.atleast_2d(K) 
         assert(np.prod(mu.shape) == K.shape[0] )
         assert(K.shape[0] == K.shape[1])
+        
         self.mu = mu
         self.K = K
+        self.dim = K.shape[0]        
         (self.Ki, self.L, self.Li, self.logdet) = pdinv(K)
+        
+        self.lpdf_const = (-0.5 * (self.dim * np.log(2 * np.pi)
+                                   + self.logdet)).flat[:]
         self.freeze = stats.multivariate_normal(mu, K)
         
     def ppf(self, component_cum_prob):
@@ -29,11 +34,27 @@ class mvnorm(object):
         return np.array(rval)
     
     def logpdf(self, x):
-        return self.freeze.logpdf(x)
+        return self.log_pdf_and_grad(x, pdf = True, grad = False)
     
     def logpdf_grad(self, x):
-        sh = (np.prod(self.mu.shape), 1)
-        return -self.Ki.dot(x.reshape(sh) - self.mu.reshape(sh)).flat[:]
+        return self.log_pdf_and_grad(x, pdf = False, grad = True)
+    
+    def log_pdf_and_grad(self, x, pdf = True, grad = True):
+        assert(pdf or grad)
+        
+        d = (x - self.mu).reshape((np.prod(self.mu.shape), 1))
+        Ki_d = self.Ki.dot(d)
+        d_Ki_d = d.T.dot(Ki_d).flat[:]
+        
+        if pdf:
+            res_pdf = self.lpdf_const - 0.5 * d_Ki_d
+            if not grad:
+                return res_pdf
+        if grad:
+            res_grad = - Ki_d.flat[:]
+            if not pdf:
+                return res_grad
+        return (res_pdf, res_grad)    
     
     def rvs(self, *args, **kwargs):
         return self.freeze.rvs(*args, **kwargs)
@@ -58,6 +79,7 @@ class mvt(object):
         self.K = K
         self.df = df
         self.dim = K.shape[0]
+        self._df_dim = self.df + self.dim
         (self.Ki, self.L, self.Li, self.logdet) = pdinv(K)
         self.freeze_mvn = stats.multivariate_normal(mu, K)
         self.freeze_chi2 = stats.chi2(self.df)
@@ -65,7 +87,7 @@ class mvt(object):
                            -(gammaln(self.df/2)
                              + (log(self.df)+log(np.pi)) * self.dim*0.5
                              + self.logdet * 0.5)
-                           )
+                           ).flat[:]
         
     def ppf(self, component_cum_prob):
         #this is a pointwise ppf
@@ -79,14 +101,27 @@ class mvt(object):
         return np.array(rval)
     
     def logpdf(self, x):
-        diff = (x - self.mu).reshape((np.prod(self.mu.shape), 1))
-        c = diff.T.dot(self.Ki).dot(diff)
-        return self.lpdf_const - 0.5*(self.df+self.dim)*log(1+c/self.df)
+        return self.log_pdf_and_grad(x, pdf = True, grad = False)
     
     def logpdf_grad(self, x):
-        diff = (x - self.mu).reshape((np.prod(self.mu.shape), 1))
-        var_diff = self.Ki.dot(diff)/self.df
-        return -(self.df + self.dim)/(1+diff.T.dot(var_diff)) * var_diff.flat[:]
+        return self.log_pdf_and_grad(x, pdf = False, grad = True)
+    
+    def log_pdf_and_grad(self, x, pdf = True, grad = True):
+        assert(pdf or grad)
+        
+        d = (x - self.mu).reshape((np.prod(self.mu.shape), 1))
+        Ki_d_scal = self.Ki.dot(d) / self.df
+        d_Ki_d_scal_1 = d.T.dot(Ki_d_scal).flat[:] + 1
+        
+        if pdf:
+            res_pdf = self.lpdf_const - 0.5 * self._df_dim * log(d_Ki_d_scal_1)
+            if not grad:
+                return res_pdf
+        if grad:
+            res_grad = -self._df_dim/d_Ki_d_scal_1 * Ki_d_scal.flat[:]
+            if not pdf:
+                return res_grad
+        return (res_pdf, res_grad)
     
     def rvs(self, n = 1):
         return self.ppf(stats.uniform.rvs(size = (n, self.dim+1)))
@@ -106,7 +141,8 @@ def test_mvt_mvn_logpdf_n_grad():
     # values from R-package bayesm, function dmvt(6.1, a, a)
     for (mu, var, df, lpdf) in [(np.array((1,1)), np.eye(2),   3, -1.83787707) ,
                                 (np.array((1,2)), np.eye(2)*3, 3, -2.93648936)]:
-        for dist in [ mvnorm(mu,var), mvt(mu,var,df)]:
+        for dist in [mvt(mu,var,df), mvnorm(mu,var)]:
             ad = np.abs(dist.logpdf(mu) -lpdf )   
             assert(ad < 10**-8)
-            assert(np.all(opt.check_grad(lambda x: dist.logpdf(x), lambda x: dist.logpdf_grad(x), mu-1) < 10**-17))
+            assert(np.all(opt.check_grad(dist.logpdf, dist.logpdf_grad, mu-1) < 10**-7))
+            #assert()
